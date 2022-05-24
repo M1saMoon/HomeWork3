82986905e28150f671affe012e3b082eeff309b0
Mira <5555159m@gmail.com>
2022-05-24 23:08:09 +0300

    add 7 task

--- Eight_element/Program.cs
+++ Eight_element/Program.cs
@@ -8,16 +8,3 @@ for (int i = 0; i < 8; i++)
     Console.Write(Binar[i] + " ");
 }
 
-
-Console.WriteLine();
-
-int[,] Matrix = new int [4,4];
-for (int i = 0; i < Matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < Matrix.GetLength(1); j++)
-    {
-        Matrix [i,j] = new Random().Next(0,2);
-        Console.Write(Matrix[i,j] + " ");
-    }
-Console.WriteLine();
-}
