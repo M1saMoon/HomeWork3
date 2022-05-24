f999dd761737d54c3af7a7af2ff58429ff141e3a
Mira <5555159m@gmail.com>
2022-05-24 20:48:39 +0300

    add 2 task

--- Cube/Program.cs
+++ Cube/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти кубы чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    //Console.WriteLine($"Куб числа {i} = {i*i*i}");
+    double result = Math.Pow(i, 3);
+    Console.WriteLine(result);
+}
