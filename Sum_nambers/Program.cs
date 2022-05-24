e51b1977ed0c0fb758e0ad29458695453851a747
Mira <5555159m@gmail.com>
2022-05-24 21:03:24 +0300

    add 3 task

--- Sum_nambers/Program.cs
+++ Sum_nambers/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Найти сумму чисел от 1 до А
+
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= a; i++) 
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {a} = {sum}");
