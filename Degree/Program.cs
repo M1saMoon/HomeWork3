8fc0e2924408104a1fac790b1d46461f4fbc904a
Mira <5555159m@gmail.com>
2022-05-24 22:10:54 +0300

    add 4 task

--- Degree/Program.cs
+++ Degree/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int result = a;
+for (int i = 2; i <= b; i++)
+result = result * a;
+Console.WriteLine($"Число {a} в степени {b} = {result}");
