79b123496e03115e39027d08d3ed2d7f2b8d6de4
Mira <5555159m@gmail.com>
2022-05-24 23:05:29 +0300

    add 7 task

--- Eight_element/Program.cs
+++ Eight_element/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+Console.Clear();
+int [] Binar = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    Binar[i] = new Random().Next(0,2);
+    Console.Write(Binar[i] + " ");
+}
+
+
+Console.WriteLine();
+
+int[,] Matrix = new int [4,4];
+for (int i = 0; i < Matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < Matrix.GetLength(1); j++)
+    {
+        Matrix [i,j] = new Random().Next(0,2);
+        Console.Write(Matrix[i,j] + " ");
+    }
+Console.WriteLine();
+}
