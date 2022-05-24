a955221f165e695824ccc89770046aa40bb34640
Mira <5555159m@gmail.com>
2022-05-24 22:45:32 +0300

    add 6 task

--- Cube_number/Program.cs
+++ Cube_number/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+    double result = Math.Pow(n, 3);
+    Console.WriteLine(result);
+}
+else 
+Console.WriteLine ($"Число {n} не заканчивается на четную цифру!");
